55ce165ed61f690001c4cf9e2fdbed0ba2bf28f0
Julio Lobo <juliolobo@lobohost.com.br>
2024-03-05 20:10:15 -0300

    Atualizar textView ao clicar no botão

--- app/src/main/java/com/example/oceanjornadaandroidmar2024/MainActivity.kt
+++ app/src/main/java/com/example/oceanjornadaandroidmar2024/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.oceanjornadaandroidmar2024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+        btEnviar.setOnClickListener {
+            tvResultado.text = "Julio Lobo!!"
+        }
     }
 }
