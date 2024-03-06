72768503f555e6db757d8b943e703917aa2bcb6e
Julio Lobo <juliolobo@lobohost.com.br>
2024-03-06 18:18:09 -0300

    Nova página criada

---
+++ app/src/main/java/com/example/oceanjornadaandroidmar2024/ResultadoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.oceanjornadaandroidmar2024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+}
