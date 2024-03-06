c02722fc5b9ea821407fbedf57656f28f6b0ad2d
Julio Lobo <juliolobo@lobohost.com.br>
2024-03-06 18:48:56 -0300

    Comportamento do botão `Voltar` adicionado

--- app/src/main/java/com/example/oceanjornadaandroidmar2024/ResultadoActivity.kt
+++ app/src/main/java/com/example/oceanjornadaandroidmar2024/ResultadoActivity.kt
@@ -2,10 +2,17 @@ package com.example.oceanjornadaandroidmar2024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ResultadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        val btVoltar = findViewById<Button>(R.id.btVoltar)
+
+        btVoltar.setOnClickListener {
+            finish()
+        }
     }
 }
